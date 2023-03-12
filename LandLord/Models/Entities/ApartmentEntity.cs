73ee2cb949cc0132e85bb194fb541dbe61d3ae8b
Peter Rydden <peter.ryden@utb.ecutbildning.se>
2023-03-12 12:06:10 +0100

    Add project files.

---
+++ LandLord/Models/Entities/ApartmentEntity.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.EntityFrameworkCore.Metadata.Internal;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.ComponentModel.DataAnnotations;
+
+namespace LandLord.Models.Entities
+{
+    public class ApartmentEntity
+    {
+        [Key]
+        public int Id { get; set; }
+        [StringLength(50)]
+        public string City { get; set; } = null!;
+        [StringLength(70)]
+        public string StreetName { get; set; } = null!;
+        [Column(TypeName = "tinyint")]
+        public byte HouseNumber { get; set; }
+        [Column(TypeName = "char(1)")]
+        public string Entrance { get; set; } = null!;
+        [Column(TypeName = "smallint")]
+        public short ApartmentNumber { get; set; }
+    }
+}
---
+++ LandLord/Models/Entities/TenantEntity.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.EntityFrameworkCore.Metadata.Internal;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.ComponentModel.DataAnnotations;
+
+namespace LandLord.Models.Entities;
+
+internal class TenantEntity
+{
+    [Key]
+    public int Id { get; set; }
+    [StringLength(50)]
+    public string FirstName { get; set; } = null!;
+    [StringLength(50)]
+    public string LastName { get; set; } = null!;
+    [Column(TypeName = "char(13)")]
+    public string PhoneNumber { get; set; } = null!;
+    [Column(TypeName = "varchar(70)")]
+    public string Email { get; set; } = null!;
+    [Required]
+    public int ApartmentId { get; set; }
+    public ICollection<ApartmentEntity> Customer = new HashSet<ApartmentEntity>();
+}
